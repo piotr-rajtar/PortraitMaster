04d07ad93b9f055a7b5ac6df8cd06c45d960c16a
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-09-16 18:17:52 +0200

    Add data escaping

--- controllers/photos.controller.js
+++ controllers/photos.controller.js
@@ -1,4 +1,5 @@
 const Photo = require('../models/photo.model');
+const escape = require('../utils').escape;
 
 /****** SUBMIT PHOTO ********/
 
@@ -21,9 +22,15 @@ exports.add = async (req, res) => {
       && author.length <= 50
       && title.length <= 25
     ) {
-      const newPhoto = new Photo({ title, author, email, src: fileName, votes: 0 });
+
+      const cleanAuthor = escape(author);
+      const cleanTitle = escape(title);
+      const cleanEmail = escape(email);
+
+      const newPhoto = new Photo({ title: cleanTitle, author: cleanAuthor, email: cleanEmail, src: fileName, votes: 0 });
       await newPhoto.save(); // ...save new photo in DB
       res.json(newPhoto);
+      
     } else {
       throw new Error('Wrong input!');
     }
