868ae49d3d06cc4f9ffed5b5b717e61526aa0ae2
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-09-16 18:18:27 +0200

    Add utils

---
+++ utils.js
@@ -0,0 +1,7 @@
+exports.escape = (text) => {
+    return text.replace(/&/g, "&amp;")
+               .replace(/</g, "&lt;")
+               .replace(/>/g, "&gt;")
+               .replace(/"/g, "&quot;")
+               .replace(/'/g, "&#039;");
+}
